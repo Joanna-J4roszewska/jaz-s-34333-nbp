b80cd49a842d4d16bada1ac9025bd2ca9ed7f59b
s34333 <s34333@pjwstk.edu.pl>
2026-06-20 12:21:50 +0200

    Initial commit

---
+++ src/test/java/jaz_s_34333_nbp/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package jaz_s_34333_nbp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
